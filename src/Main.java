4a0d9cde74f8b2e33b0f00394fb8111a1cd6fefe
Иван Зыкин <faustganibal4@gmail.com>
2022-04-04 20:40:33 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int value;
+
+        System.out.print("Введите целое не отрицательное число: ");
+        value = new Scanner(System.in).nextInt();
+    }
+}
