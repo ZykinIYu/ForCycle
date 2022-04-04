7f651f6d1eba6dd5cc3749a7a2ae50d067859ea5
Иван Зыкин <faustganibal4@gmail.com>
2022-04-04 20:51:42 +0600

    цикл для вычисления цисел готов

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int value;
+        int value = 0;
 
         System.out.print("Введите целое не отрицательное число: ");
         value = new Scanner(System.in).nextInt();
+
+        if (value == 0) {
+            System.out.println("Введено цисло 0, вычислять нечего");
+        }
+
+        for (int i = 1; i <= value; i++) {
+            if (value % i == 0) {
+                System.out.println(i + " * " + value / i);
+            }
+        }
     }
 }
